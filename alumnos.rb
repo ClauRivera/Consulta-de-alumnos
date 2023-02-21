f2c21f01ca89924382a650d74b2169c3346283a2
Claudia Rivera <121994517+ClauRivera@users.noreply.github.com>
2023-02-21 14:48:57 -0300

    Add files via upload
    Inicio de un proyecto de consulta de notas de un colegio

---
+++ alumnos.rb
@@ -0,0 +1,18 @@
+# práctica: un sistema que permita a los estudiantes consultar su promedio de notas ingresando 3 calificaciones 
+
+puts "Ingrese primer nota"
+nota1 = gets.to_i
+puts "Ingrese segunda nota"
+nota2 = gets.to_i
+puts "Ingrese tercer nota"
+nota3 = gets.to_i
+prom = (nota1 + nota2 + nota3) / 3
+if prom >= 7
+  puts "Promocionado"
+else
+  if prom >= 4
+    puts "Regular"
+  else
+    puts "Reprobado"
+  end
+end
